300cc93a50f5ce1be5c26312fbbc8a63486fff99
Simon Praetorius <simon.praetorius@tu-dresden.de>
2020-09-21 18:16:28 +0200

    remove CXX_MAX_STANDARD and CXX_MAX_SUPPORTED_STANDARD and deprecate dune_require_cxx_standard
    add CHANGELOG entry
    
    link pybind11 module explicitly against dunecommon
    
    add cmake-check for equal c++ standard of targets
    
    add CXX_STANDARD to feature test macros
    
    remove c++ standard related unnecessary parameters in gitlabci.yml
    
    Cleanup CheckCXXFeatures and add documentation of dune_check_cxx_source_compiles macro

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -29,6 +29,13 @@
 
 - Deprecate cmake function `remove_processed_modules`
 
+- Setting the minimal c++ standard in cmake is now done by a cmake feature-requirement
+  `cxx_std_17` on the `dunecommon` library target. This requirement is propagated to all
+  other modules by linking against `dunecommon`. The cmake options `CXX_MAX_STANDARD`,
+  `CXX_MAX_SUPPORTED_STANDARD` and `DISABLE_CXX_VERSION_CHECK` are removed.
+
+- The cmake function `dune_require_cxx_standard()` is deprecated.
+
 ## Deprecations and removals
 
 - The deprecated headers `gcd.hh` and `lcm.hh` are removed. Use `std::gcd`
--- cmake/modules/DuneMacros.cmake
+++ cmake/modules/DuneMacros.cmake
@@ -1,28 +1,38 @@
-# Core DUNE module for CMake.
-#
-# Documentation of the public API defined in this module:
-#
-# .. cmake_function:: dune_target_link_libraries
-#
-#    .. cmake_param:: BASENAME
-#
-#    .. cmake_param:: LIBRARIES
-#
-#    Link libraries to the static and shared version of
-#    library BASENAME
-#
-#
-# .. cmake_function:: add_dune_all_flags
-#
-#    .. cmake_param:: targets
-#       :single:
-#       :required:
-#       :positional:
-#
-#       The targets to add the flags of all external libraries to.
-#
-#    This function is superseded by :ref:`dune_target_enable_all_packages`.
-#
+#[=======================================================================[.rst:
+DuneMacros
+----------
+
+Core DUNE module for CMake.
+
+
+.. cmake:command:: target_link_dune_default_libraries
+
+  .. code-block:: cmake
+
+    target_link_dune_default_libraries(<target>)
+
+  Alias for ``target_link_libraries(<target> PUBLIC ${DUNE_LIBS})``
+
+
+.. cmake:command:: dune_target_link_libraries
+
+  .. code-block:: cmake
+
+    dune_target_link_libraries(<target> <library>)
+
+  Alias for ``target_link_libraries(<target> PUBLIC <library>)``
+
+
+.. cmake:command:: add_dune_all_flags
+
+  .. code-block:: cmake
+
+    add_dune_all_flags(<target>)
+
+  Add the flags of all registered packages to the given ``<target>``.
+  This function is superseded by :command:`dune_target_enable_all_packages`.
+
+#]=======================================================================]
 include_guard(GLOBAL)
 
 enable_language(C) # Enable C to skip CXX bindings for some tests.
@@ -43,17 +53,25 @@ include(DuneSymlinkOrCopy)
 include(DuneTestMacros)
 include(DuneUtilities)
 
+# deprecated
+function(dune_target_link_libraries _target _lib)
+  message(DEPRECATION "dune_target_link_libraries is deprecated. Use "
+    "'target_link_libraries(<target> PUBLIC <lib>)' instead.")
+  target_link_libraries(${_target} PUBLIC ${_lib})
+endfunction(dune_target_link_libraries)
+
+# deprecated
 macro(target_link_dune_default_libraries _target)
-  foreach(_lib ${DUNE_LIBS})
-    target_link_libraries(${_target} PUBLIC ${_lib})
-  endforeach()
+  message(DEPRECATION "target_link_dune_default_libraries is deprecated. Use "
+    "'target_link_libraries(<target> PUBLIC \${DUNE_LIBS})' instead.")
+  target_link_libraries(${_target} PUBLIC ${DUNE_LIBS})
 endmacro(target_link_dune_default_libraries)
 
-function(dune_target_link_libraries basename libraries)
-  target_link_libraries(${basename} PUBLIC ${libraries})
-endfunction(dune_target_link_libraries basename libraries)
-
+# deprecated
 macro(add_dune_all_flags targets)
+  message(DEPRECATION "add_dune_all_flags is deprecated. Use "
+    "'dune_target_enable_all_packages(<targets>...)' instead.")
+
   get_property(incs GLOBAL PROPERTY ALL_PKG_INCS)
   get_property(defs GLOBAL PROPERTY ALL_PKG_DEFS)
   get_property(libs GLOBAL PROPERTY ALL_PKG_LIBS)
@@ -65,3 +83,15 @@ macro(add_dune_all_flags targets)
     target_compile_options(${target} PUBLIC ${opts})
   endforeach()
 endmacro(add_dune_all_flags targets)
+
+
+# find a package if not already found
+macro(dune_find_package _name)
+  cmake_parse_arguments(DUNE_PKG "" "TARGET" "" ${ARGN})
+
+  # TODO: This does not yet work correctly
+  if((NOT DEFINED ${_name}_FOUND OR ${_name}_FOUND) AND
+     (NOT DUNE_PKG_TARGET OR NOT TARGET ${DUNE_PKG_TARGET}))
+    find_package(${_name} ${DUNE_PKG_UNPARSED_ARGUMENTS})
+  endif()
+endmacro(dune_find_package)
