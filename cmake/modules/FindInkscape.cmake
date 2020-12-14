27090b471a676adc013f3cd29c1c5e6a84eeb6cf
Linus Seelinger <linus.seelinger@iwr.uni-heidelberg.de>
2020-12-14 10:24:06 +0100

    Disable inkscape

--- cmake/modules/FindInkscape.cmake
+++ cmake/modules/FindInkscape.cmake
@@ -11,7 +11,7 @@
 #       Path to inkscape to generate .png's form .svg's
 #
 
-find_program(INKSCAPE inkscape DOC "Path to inkscape to generate png files from svg files")
+#find_program(INKSCAPE inkscape DOC "Path to inkscape to generate png files from svg files")
 find_program(CONVERT convert DOC "Path to convert program")
 if(INKSCAPE)
   set(INKSCAPE_FOUND True)
