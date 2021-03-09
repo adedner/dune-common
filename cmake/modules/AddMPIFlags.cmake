8eea295b102d895998651fb5ea08569539b20494
Simon Praetorius <simon.praetorius@tu-dresden.de>
2021-03-09 09:55:33 +0100

    Remove unnecessary comment

--- cmake/modules/AddMPIFlags.cmake
+++ cmake/modules/AddMPIFlags.cmake
@@ -23,14 +23,13 @@ if(MPI_C_FOUND)
   # provide flags to manually disable c++ bindings in MPI_C-library
   set(MPI_DISABLE_CXX_BINDINGS MPICH_SKIP_MPICXX OMPI_SKIP_MPICXX MPIPP_H MPI_NO_CPPBIND)
 
-  # TODO check on where to position this exactly, doesn't look completely thought through
   dune_register_package_flags(COMPILE_DEFINITIONS "ENABLE_MPI=1;${MPI_DISABLE_CXX_BINDINGS}"
                               LIBRARIES MPI::MPI_C)
 endif()
 
 # adds MPI flags to the targets
 function(add_dune_mpi_flags)
-  cmake_parse_arguments(ADD_MPI "SOURCE_ONLY;OBJECT" "" "" ${ARGN})
+  cmake_parse_arguments(ADD_MPI "SOURCE_ONLY;OBJECT" "" "" ${ARGN}) # ignored
   set(targets ${ADD_MPI_UNPARSED_ARGUMENTS})
 
   if(MPI_C_FOUND)
