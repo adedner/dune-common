f36078c81c6ff700c8f6274111b06ad5a6061ed2
Carsten Gräser <graeser@dune-project.org>
2021-10-25 11:53:49 +0200

    [test][bugfix] Define FieldTraits for test implementation of a DenseVector
    This is a bugfix. By not defining the traits class it implicitly used
    the default implementation which did not make any sense at all.
    
    BTW:
    It's a pitty that we have to define two traits classes for any
    `DenseVector` implementation `DenseMatVecTraits` and `FieldTraits`.
    But fixing this is another issue.

--- dune/common/test/fvectorconversion1d.cc
+++ dune/common/test/fvectorconversion1d.cc
@@ -35,6 +35,10 @@ namespace Dune
     using size_type = std::size_t;
   };
 
+  template<class Component, std::size_t Dim >
+  struct FieldTraits< MyVector<Component, Dim> > : public FieldTraits<DenseVector<MyVector<Component, Dim>>>
+  {};
+
   template<class Component, std::size_t Dim, int Size>
   struct IsFieldVectorSizeCorrect<MyVector<Component, Dim>, Size>
     : std::integral_constant<bool, Dim == Size>
