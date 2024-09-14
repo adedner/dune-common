5ee5f50eac3b61c41ce2c43c55d0747080d046ed
Simon Praetorius <simon.praetorius@tu-dresden.de>
2024-09-14 23:05:13 +0200

    Improve the documentation of vector and add CMakeLists entries

--- dune/common/vector.hh
+++ dune/common/vector.hh
@@ -12,11 +12,34 @@
 #include <stdexcept>
 #include <type_traits>
 
+#include <dune/common/assert.hh>
+#include <dune/common/std/no_unique_address.hh>
+
 namespace Dune {
 
-template <class Element, class Allocator = std::allocator<Element>>
-class Vector 
+/**
+ * \brief Vector is a sequence container that encapsulates dynamic size arrays.
+ *
+ * This sequence container data structure is similar to the `std::vector` container
+ * without the inremental size-changing operations, like `push_back`. The motivation
+ * for developing our own data container is the unfortunate specialization of the
+ * `std::vector<bool>` with proxy reference types. This leads to lots of codes where
+ * boolean values are to be represented as `char` or `int`.
+ *
+ * This container is a more restrictive than the `std::vector` implementation. We
+ * do not allow incremental size changes. The `resize()` function creates a new
+ * storage even if the size is reduced.
+ *
+ * \tparam Element The type of the elements stored in the conatainer.
+ * \tparam An allocator that is used to acquire/release memory and to construct/destroy
+ *         the elements in that memory.
+ */
+template <class Element,
+          class Allocator = std::allocator<Element>>
+class Vector
 {
+  static_assert(std::is_same_v<Element, typename Allocator::value_type>);
+
 public:
   using ElementType = Element;
   using ValueType = std::remove_const_t<ElementType>;
@@ -35,19 +58,23 @@ public:
   /// \name Constructors and Destructor
   /// @{
 
+  /// \brief Construct an empty vector and default construct the allocator.
   constexpr Vector () noexcept( noexcept(AllocatorType()) )
   {}
 
+  /// \brief Construct an empty vector and construct the allocator from the argument.
   constexpr explicit Vector (const AllocatorType& alloc) noexcept
     : alloc_(alloc)
   {}
 
+  /// \brief Construct a vector of size `size` using the default or provided allocator
   constexpr explicit Vector (SizeType size, const AllocatorType& alloc = AllocatorType())
     : size_(size)
     , alloc_(alloc)
     , data_(size_ > 0 ? alloc_.allocate(size_) : nullptr)
   {}
 
+  /// \brief Construct a vector of size `size` with all element initialized to `value`
   constexpr Vector (SizeType size, ValueType value, const AllocatorType& alloc = AllocatorType())
     : size_(size)
     , alloc_(alloc)
@@ -57,6 +84,7 @@ public:
       data_[i] = value;
   }
 
+  /// \brief Construct the vector from an iterator range `[first, last)`
   template <class InputIt,
     decltype(*std::declval<InputIt>(), ++std::declval<InputIt>(), bool{}) = true>
   constexpr Vector (InputIt first, InputIt last, const AllocatorType& alloc = AllocatorType())
@@ -66,10 +94,12 @@ public:
       data_[i] = *first;
   }
 
+  /// \brief Construct a copy of the vector `other`
   constexpr Vector (const Vector& other)
     : Vector(other, other.alloc_)
   {}
 
+  /// \brief Construct a copy of the vector `other`using a provided allocator
   constexpr Vector (const Vector& other, const AllocatorType& alloc)
     : Vector(other.size_, alloc)
   {
@@ -77,12 +107,15 @@ public:
       data_[i] = other[i];
   }
 
+  /// \brief Move construct the vector from `other` leaving `other` in an empty state
   constexpr Vector (Vector&& other) noexcept
     : Vector()
   {
     this->swap(other);
   }
 
+  /// \brief Move construct the vector from using using a provided allocator by
+  /// move assigning all element to the new allocated memory.
   constexpr Vector (Vector&& other, const AllocatorType& alloc)
     : Vector(other.size_, alloc)
   {
@@ -90,10 +123,12 @@ public:
       data_[i] = std::move(other[i]);
   }
 
+  /// \brief Constrct the vector from the provided initializer list
   constexpr Vector (std::initializer_list<ElementType> init, const AllocatorType& alloc = AllocatorType())
     : Vector(init.begin(), init.end(), alloc)
   {}
 
+  /// \brief The destructor frees all allocated memory
   ~Vector () noexcept
   {
     if (data_)
@@ -106,6 +141,7 @@ public:
   /// \name Assignment
   /// @{
 
+  /// \brief Copy-assign `other` to this
   constexpr Vector& operator= (const Vector& other)
   {
     Vector that(other);
@@ -113,6 +149,7 @@ public:
     return *this;
   }
 
+  /// \brief Move assign `other` to this.
   constexpr Vector& operator= (Vector&& other)
   {
     Vector that(std::move(other));
@@ -120,33 +157,28 @@ public:
     return *this;
   }
 
-  constexpr void swap (Vector& other)
-  {
-    using std::swap;
-    swap(size_, other.size_);
-    swap(alloc_, other.alloc_);
-    swap(data_, other.data_);
-  }
-
+  /// \brief Replaces the contents with `count` copies of value `value`.
   constexpr void assign (SizeType count, const ElementType& value)
   {
-    Vector tmp(count, value);
-    this->swap(tmp);
+    for (SizeType i = 0; i < std::min(count,size_); ++i)
+      data_[i] = value;
   }
 
+  /// \brief Replaces the contents with copies of those in the range `[first, last)`.
   template <class InputIt>
   constexpr void assign (InputIt first, InputIt last)
   {
-    Vector tmp(first, last);
-    this->swap(tmp);
+    for (SizeType i = 0; i < size_ && first != last;  ++i, ++first)
+      data_[i] = *first;
   }
 
+  /// \brief Replaces the contents with the elements from the initializer list `ilist`.
   constexpr void assign (std::initializer_list<ElementType> ilist)
   {
-    Vector tmp(std::move(ilist));
-    this->swap(tmp);
+    assert(ilist.begin(), ilist.end());
   }
 
+  /// \brief Returns the associated allocator.
   constexpr AllocatorType get_allocator () const noexcept
   {
     return alloc_;
@@ -155,60 +187,72 @@ public:
   /// @}
 
 
-  /// \name Element access 
+  /// \name Element access
   /// @{
 
+  /// \brief Access specified element at position `pos` with bounds checking
   constexpr Reference at (SizeType pos)
   {
-    if (0 <= pos && pos < size_)
+    if (indexInRange(pos))
       return data_[pos];
-    else 
+    else
       throw std::out_of_range("Index out of range.");
   }
 
+  /// \brief Access specified element at position `pos` with bounds checking
   constexpr ConstReference at (SizeType pos) const
   {
-    if (0 <= pos && pos < size_)
+    if (indexInRange(pos))
       return data_[pos];
-    else 
+    else
       throw std::out_of_range("Index out of range.");
   }
 
+  /// \brief Access specified element at position `pos`
   constexpr Reference operator[] (SizeType pos) noexcept
   {
+    DUNE_ASSERT_MSG(indexInRange(pos), "Index out of range.");
     return data_[pos];
   }
 
+  /// \brief Access specified element at position `pos`
   constexpr ConstReference operator[] (SizeType pos) const noexcept
   {
+    DUNE_ASSERT_MSG(indexInRange(pos), "Index out of range.");
     return data_[pos];
   }
 
-  constexpr Reference front (SizeType pos) noexcept
+  /// \brief Access the first element
+  constexpr Reference front () noexcept
   {
     return data_[0];
   }
 
-  constexpr ConstReference front (SizeType pos) const noexcept
+  /// \brief Access the first element
+  constexpr ConstReference front () const noexcept
   {
     return data_[0];
   }
 
-  constexpr Reference back (SizeType pos) noexcept
+  /// \brief Access the last element
+  constexpr Reference back () noexcept
   {
     return data_[size_-1];
   }
 
-  constexpr ConstReference back (SizeType pos) const noexcept
+  /// \brief Access the last element
+  constexpr ConstReference back () const noexcept
   {
     return data_[size_-1];
   }
 
+  /// \brief Direct access to the underlying contiguous storage
   constexpr ElementType* data () noexcept
   {
     return data_;
   }
 
+  /// \brief Direct access to the underlying contiguous storage
   constexpr const ElementType* data () const noexcept
   {
     return data_;
@@ -220,31 +264,37 @@ public:
   /// \name Iterators
   /// @{
 
-  constexpr Iterator begin () noexcept 
+  /// \brief Returns an iterator to the beginning
+  constexpr Iterator begin () noexcept
   {
     return data_;
   }
-  
+
+  /// \brief Returns an iterator to the beginning
   constexpr ConstIterator begin () const noexcept
   {
     return data_;
   }
 
+  /// \brief Returns an iterator to the beginning
   constexpr ConstIterator cbegin () const noexcept
   {
     return data_;
   }
 
-  constexpr Iterator end () noexcept 
+  /// \brief Returns an iterator to the end
+  constexpr Iterator end () noexcept
   {
     return data_ + size_;
   }
-  
+
+  /// \brief Returns an iterator to the end
   constexpr ConstIterator end () const noexcept
   {
     return data_ + size_;
   }
 
+  /// \brief Returns an iterator to the end
   constexpr ConstIterator cend () const noexcept
   {
     return data_ + size_;
@@ -256,41 +306,84 @@ public:
   /// \name Capacity
   /// @{
 
-  [[nodiscard]] constexpr bool empty () const noexcept 
+  /// \brief Checks whether the container is empty
+  [[nodiscard]] constexpr bool empty () const noexcept
   {
     return size_ == 0 || data_ == nullptr;
   }
 
-  [[nodiscard]] constexpr SizeType size () const noexcept 
+  /// \brief Returns the number of elements
+  [[nodiscard]] constexpr SizeType size () const noexcept
   {
     return size_;
   }
 
-  [[nodiscard]] constexpr SizeType max_size () const noexcept 
+  /// \brief Returns the maximum possible number of elements
+  [[nodiscard]] constexpr SizeType max_size () const noexcept
   {
     return std::numeric_limits<DifferenceType>::max();
   }
 
-  constexpr void reserve (SizeType new_cap) noexcept
-  {
-    /* do nothing */
-  }
-
+  /// \brief Returns the number of elements that can be held in currently allocated storage.
   [[nodiscard]] constexpr SizeType capacity () const noexcept
   {
     return size_;
   }
 
-  constexpr void shrink_to_fit () noexcept
+  /// @}
+
+
+  /// \name Modifiers
+  /// @{
+
+  /// \brief Clears the contents
+  constexpr void clear () noexcept
   {
-    /* do nothing */
+    resize(0);
+  }
+
+  /// \brief Changes the number of elements stored. If `count != size` allocate new storage.
+  void resize (SizeType count)
+  {
+    if (size_ != count) {
+      Vector tmp(count);
+      this->swap(tmp);
+    }
+  }
+
+  /// \brief Changes the number of elements stored and set the value of all elements to `value`.
+  /// If `count != size` allocate new storage with the given initial value.
+  void resize (SizeType count, const ElementType& value)
+  {
+    if (size_ != count) {
+      Vector tmp(count, value);
+      this->swap(tmp);
+    }
+  }
+
+  /// \brief Swaps the contents with `other`
+  constexpr void swap (Vector& other)
+  {
+    using std::swap;
+    swap(size_, other.size_);
+    swap(alloc_, other.alloc_);
+    swap(data_, other.data_);
   }
 
   /// @}
 
 private:
+  [[always_inline]] inline bool indexInRange (SizeType pos) const
+  {
+    if constexpr (std::is_signed_v<SizeType>)
+      return 0 <= pos && pos < size_;
+    else
+      return pos < size_;
+  }
+
+private:
   SizeType size_ = 0;
-  AllocatorType alloc_ = {};
+  DUNE_NO_UNIQUE_ADDRESS AllocatorType alloc_ = {};
   ElementType* data_ = nullptr;
 };
 
